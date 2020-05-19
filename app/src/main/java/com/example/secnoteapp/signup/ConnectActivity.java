269072d87c8742dbe68c4b5e8a7595b51aa77842
B-Yahia <60832087+B-Yahia@users.noreply.github.com>
2020-05-19 15:10:50 +0300

    final version

--- app/src/main/java/com/example/secnoteapp/signup/ConnectActivity.java
+++ app/src/main/java/com/example/secnoteapp/signup/ConnectActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -19,10 +22,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.prefs.Preferences;
+
 public class ConnectActivity extends AppCompatActivity {
 
     private Button connectBtn;
     private EditText userEmail,userPassword;
+    private CheckBox mCheckBox;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor spEditor;
 
     private ProgressBar progressBar;
     private FirebaseAuth firebaseAuth;
@@ -37,6 +46,12 @@ public class ConnectActivity extends AppCompatActivity {
         userEmail = findViewById(R.id.user_email);
         userPassword = findViewById(R.id.user_password);
         progressBar = findViewById(R.id.progressBar);
+        mCheckBox = findViewById(R.id.checkBox);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        spEditor = sharedPreferences.edit();
+        checkSharedPreferences();
+
 
         firebaseAuth = FirebaseAuth.getInstance();
         connectBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +59,35 @@ public class ConnectActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = userEmail.getText().toString();
                 String password = userPassword.getText().toString();
+                if (mCheckBox.isChecked()){
+                    spEditor.putString(getString(R.string.checkbox), "True");
+                    spEditor.commit();
+
+                    String emailtosave = userEmail.getText().toString();
+                    spEditor.putString(getString(R.string.email),emailtosave);
+                    spEditor.commit();
+
+                    String passwordtosave = userPassword.getText().toString();
+                    spEditor.putString(getString(R.string.password),passwordtosave);
+                    spEditor.commit();
+                } else {
+                    spEditor.putString(getString(R.string.checkbox), "False");
+                    spEditor.commit();
+
+
+                    spEditor.putString(getString(R.string.email),"");
+                    spEditor.commit();
+
+
+                    spEditor.putString(getString(R.string.password),"");
+                    spEditor.commit();
+
+                }
+
                 updateUI(email,password);
             }
+
+
         });
     }
     private void updateUI(String uEmail,String uPassword){
@@ -76,6 +118,21 @@ public class ConnectActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    private void checkSharedPreferences (){
+        String checkBox = sharedPreferences.getString(getString(R.string.checkbox),"false");
+        String email = sharedPreferences.getString(getString(R.string.email),"");
+        String password = sharedPreferences.getString(getString(R.string.password),"");
+
+        userEmail.setText(email);
+        userPassword.setText(password);
+        if (checkBox.equals(true)){
+            mCheckBox.setChecked(true);
+        } else {
+            mCheckBox.setChecked(false);
+        }
 
     }
 }
